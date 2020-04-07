7140158f2e9befae0ae3c821ead85608b223762b
Flynn <shannon1@kenyon.edu>
2020-04-07 16:24:17 -0400

    add comment

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from tree import Tree
 import web
 
+# code for reading from spreadsheet will go here
+
 test = Tree('106 Gaskin Avenue Gambier, Ohio 43022', ['red maple', '//*[@id="species-option-container"]/span[7]'], '20.4', '//*[@id="condition"]/option[2]',
-            '//*[@id="exposure"]/option[2]', True, '//*[@id="vintage"]/option[2]', '//*[@id="distance"]/option[2]', '//*[@id="direction"]/option[2]')
+            '//*[@id="exposure"]/button[3]', True, '//*[@id="vintage"]/option[3]', '//*[@id="distance"]/option[2]', '//*[@id="direction"]/option[2]')
 web.enter_tree(test)
